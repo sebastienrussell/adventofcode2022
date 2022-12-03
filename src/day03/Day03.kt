a47fc87ffecdf9adefe4e3ae57dab5efabf8fd39
Sebastien Russell <russell.sebas@gmail.com>
2022-12-03 10:28:32 -0500

    Day03

---
+++ src/day03/Day03.kt
@@ -0,0 +1,53 @@
+package day03
+
+import models.PRIORITIES
+import models.Rucksack
+import models.RucksackGroup
+import readInput
+
+fun main() {
+    fun parse(input: List<String>) = input.map { Rucksack(items = it) }
+
+    fun part1(input: List<Rucksack>): Int {
+        val itemsInBoth = mutableListOf<Pair<Char, Int>>()
+        input.forEach { rucksack ->
+            val rucksackDivided = rucksack.divide()
+            val itemInBoth = rucksackDivided[0].findCommonCharacter(rucksackDivided[1])
+            itemsInBoth.add(Pair(itemInBoth, itemInBoth.getPriority()))
+        }
+        return itemsInBoth.sumOf { it.second }
+    }
+
+    fun parseRucksackGroups(input: List<Rucksack>): MutableList<RucksackGroup> {
+        val groupsOfRucksack = mutableListOf<RucksackGroup>()
+        val inputIterator = input.iterator()
+        while (inputIterator.hasNext()) {
+            groupsOfRucksack.add(
+                RucksackGroup(
+                    first = inputIterator.next(),
+                    second = inputIterator.next(),
+                    third = inputIterator.next()
+                )
+            )
+        }
+        return groupsOfRucksack
+    }
+
+    fun part2(input: List<Rucksack>): Int =
+        parseRucksackGroups(input).sumOf { it.getPriority() }
+
+
+    val input = readInput("Day03", packageName = "day03")
+    val parsedInputs = parse(input)
+    println(part1(parsedInputs))
+    println(part2(parsedInputs))
+}
+
+fun String.findCommonCharacter(other: String) =
+    first { other.indexOf(it) != -1 }
+
+fun String.findCommonCharacter(second: String, third: String) =
+    first { second.indexOf(it) != -1 && third.indexOf(it) != -1 }
+
+fun Char.getPriority() =
+    PRIORITIES.indexOf(this) + 1
---
+++ src/models/Rucksack.kt
@@ -0,0 +1,27 @@
+package models
+
+import day03.findCommonCharacter
+import day03.getPriority
+
+data class Rucksack(
+    val items: String
+) {
+    fun divide() =
+        listOf(
+            items.substring(0 until items.length / 2),
+            items.substring(items.length / 2)
+        )
+}
+
+data class RucksackGroup(
+    val first: Rucksack,
+    val second: Rucksack,
+    val third: Rucksack
+) {
+    fun getPriority() =
+        first.items.findCommonCharacter(second.items, third.items).getPriority()
+}
+
+
+// find index here to get the priority
+const val PRIORITIES = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
